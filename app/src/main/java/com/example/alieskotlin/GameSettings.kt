5e7655ae40271462a5ba90c9dc3f58b788e2bd13
User <shikhert666@gmail.com>
2020-02-23 23:31:21 +0300

    edited a bit

--- app/src/main/java/com/example/alieskotlin/GameSettings.kt
+++ app/src/main/java/com/example/alieskotlin/GameSettings.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_game_settings.*
 
@@ -20,9 +22,37 @@ class GameSettings : Fragment() {
     }
 
     override fun onStart() {
+
+        var selectedTime : String
+
         moveToWordListChoosing.setOnClickListener {
             findNavController().navigate(R.id.action_gameSettings_to_wordListChoosing)
         }
+
+        val timeChoose = arrayOf("30 sec", "1 min", "2 min", "4 min")
+        val arrayAdapter =
+            context?.let { ArrayAdapter<String>(it, android.R.layout.simple_spinner_dropdown_item, timeChoose) }
+
+        spinner_timeChoosing.adapter = arrayAdapter
+
+        spinner_timeChoosing.onItemSelectedListener = object :
+
+        AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            selectedTime = "1 min"
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+            selectedTime = timeChoose[position]
+            }
+
+        }
+
         super.onStart()
     }
 
